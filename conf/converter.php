5a44751360973e47cb26a9c5d08e975d21f69c4d
Dominik Eckelmann <eckelmann@karsten.cosmo>
2010-01-12 12:54:44 +0100

    config file is now empty on default

--- conf/converter.php
+++ conf/converter.php
@@ -1,10 +1,10 @@
 #<?php die() ?>
 # PHP include hack
 
-# 
+#
 # Use this file to setup the document to text converter.
 #
-# The plugin trys to convert every media document to a text file. On this 
+# The plugin trys to convert every media document to a text file. On this
 # progress it uses a given set of external tools to convert it.
 # This tools are defined per file extension.
 #
@@ -13,9 +13,5 @@
 #
 # example
 #
-#   pdf /usr/bin/pdftotext %in% %out% 
-# 
-
-
-pdf				/usr/bin/pdftotext %in% %out%
-doc				/usr/bin/antiword %in% > %out%
+#pdf				/usr/bin/pdftotext %in% %out%
+#doc				/usr/bin/antiword %in% > %out%
