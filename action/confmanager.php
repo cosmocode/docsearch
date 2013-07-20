46055ac3d5aa8643ee8d04fea028f505a4ca67b8
Dominik Eckelmann <deckelmann@gmail.com>
2013-07-20 14:05:56 +0200

    use confmanager to setup the convert config

---
+++ action/confmanager.php
@@ -0,0 +1,26 @@
+<?php
+
+class action_plugin_docsearch_confmanager extends DokuWiki_Action_Plugin {
+
+    public function register(&$controller) {
+        $controller->register_hook('CONFMANAGER_CONFIGFILES_REGISTER', 'BEFORE',  $this, 'addConfigFile', array());
+    }
+
+    public function addConfigFile(Doku_Event $event, $params) {
+        if (class_exists('ConfigManagerTwoLine')) {
+            $config = new ConfigManagerTwoLine($this->getLang('confmanager title'), $this->getDescription(), DOKU_PLUGIN . 'docsearch/conf/converter.php');
+            $event->data[] = $config;
+        }
+    }
+
+    private function getDescription() {
+        $fn = $this->localFN('confmanager_description');
+        msg($fn);
+        if (!@file_exists($fn)) {
+            return '';
+        }
+        $content = file_get_contents($fn);
+        return $content;
+    }
+
+}
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,5 +1,6 @@
 <?php
 
 $lang['title'] = 'Results in Documents';
+$lang['confmanager title'] = 'DocSearch'
 
 ?>
