2642880225f70e65477ffb8bb6e194e1b33e7575
PPPCR <c.raeuber@3p-engineering.de>
2015-04-12 17:15:40 +0200

    Update search.php
    Takes over the search query (highlighted words only) from dokuwiki to PDF as described in http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf
    At the moment, at least Adobe Reader and PDF XChange Viewer support this. ATM firefox's internal PDF viewer pdf.js doesn't seem to support this yet, but bug reports already exist.

--- action/search.php
+++ action/search.php
@@ -66,7 +66,12 @@ class action_plugin_docsearch_search extends DokuWiki_Action_Plugin {
             if ($isPDF === false) {
                 echo '<a href="' . ml($id) . '" title="" class="wikilink1">' . hsc($id) . '</a>:';
             } else {
-                echo '<a href="' . ml($id) . '#search=' . $QUERY . '" title="" class="wikilink1">' . hsc($id) . '</a>:';
+            	$Indexer = idx_get_indexer();
+            	$q = ft_queryParser($Indexer, $QUERY);
+            	$highlight = $q['highlight'];
+            	$wordList = urlencode(join(" ", $highlight));
+            	
+            	echo '<a href="' . ml($id) . '#search=&quot;' . $wordList . '&quot;" title="" class="wikilink1">' . hsc($id) .'</a>:';
             }
 
             echo '<span class="search_cnt">' . hsc($data['hits']) . ' ' . hsc($lang['hits']) . '</span>';
