87fcc531c17a03fb4e47dd81d3f6d929cbcc598f
serg <sergey_art82@inbox.ru>
2015-06-24 00:15:05 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author serg <sergey_art82@inbox.ru>
+ */
+$lang['title']                 = 'Результаты поиска в документах';
+$lang['confmanager title']     = 'DocSearch';
