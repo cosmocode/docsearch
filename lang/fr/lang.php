77aef5ad13d93b3c6536222fdf23f2b91309dda8
Corto Carde <corto.carde@laposte.net>
2016-05-10 17:20:05 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Corto Carde <corto.carde@laposte.net>
+ */
+$lang['title']                 = 'Résultats dans Documents';
