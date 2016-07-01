fa3cf9f7289749d702a5366da1f3c5780dd07976
Myeongjin <aranet100@gmail.com>
2016-07-01 12:05:47 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['showSnippets']          = '결과의 처음 #번째에서 스니펫 보이기';
+$lang['showUsage']             = '결과의 처음 #번째에서 파일 사용법 보이기';
