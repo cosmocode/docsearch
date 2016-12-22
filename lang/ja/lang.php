f41a0ff82cdb5f046bd86d77bf71cc65559cb6f6
Hideaki SAWADA <chuno@live.jp>
2016-12-22 05:50:12 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -2,8 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['title']                 = 'ドキュメントの結果';
 $lang['confmanager title']     = 'DocSearch';
+$lang['usage']                 = '使用方法';
