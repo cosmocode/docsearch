e5a0ff6b9293ce7ae401e090b6c55c91507dea04
ubibene <services.m@benard.info>
2017-05-23 22:30:29 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,7 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Corto Carde <corto.carde@laposte.net>
+ * @author ubibene <services.m@benard.info>
  */
 $lang['title']                 = 'Résultats dans Documents';
+$lang['confmanager title']     = 'DocSearch';
+$lang['usage']                 = 'Utilisation';
---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author ubibene <services.m@benard.info>
+ */
+$lang['showSnippets']          = 'Montrer des extrais des # premiers résultats';
+$lang['showUsage']             = 'Montrer l\'utilisation par les fichiers des # premiers résultats';
