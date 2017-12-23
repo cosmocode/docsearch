e02a25203372efaa5c0543b5fe7515cb23e8a7c7
Tor Härnqvist <tor@harnqvist.se>
2017-12-23 13:35:17 +0100

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['showSnippets']          = 'Visa utdrag av de första # resultaten';
+$lang['showUsage']             = 'Visa fil-användade för de första # resultaten';
