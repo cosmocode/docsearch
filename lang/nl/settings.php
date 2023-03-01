2ae8426b24f3f3dc28f4e3cb14ce50e8a97d4a9a
Gerrit <klapinklapin@gmail.com>
2023-03-01 21:20:24 +0100

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Rene <wllywlnt@yahoo.com>
  */
 $lang['showSnippets']          = 'Toon knipsels van de eerste # resultaten';
